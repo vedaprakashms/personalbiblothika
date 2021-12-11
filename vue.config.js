5aa1bf61187a5bb9f90b886ef7db4e4717451852
Vedaprakash <vedaprakashms@gmail.com>
2021-12-11 20:36:55 +0530

    update

--- vue.config.js
+++ vue.config.js
@@ -1,53 +1,56 @@
-import WorkerPlugin from 'worker-plugin'
-export const configureWebpack = {
-    plugins: [new WorkerPlugin()],
-}
-export const pluginOptions = {
-    electronBuilder: {
-        nodeIntegration: true,
-        nodeGypRebuild: true,
-        externals: [
-            'mongoose',
-            'mongodb',
-            'vue3-qrcode-reader',
-            'vue-toastification',
-        ],
-        homepage: 'https://github.com/vedaprakashms',
-        builderOptions: {
-            // options placed here will be merged with default configuration and passed to electron-builder
-            appId: 'io.personalbiblothika.desktop',
-            productName: 'peronal Lib App',
-            linux: {
-                target: ['deb', 'AppImage', '7z'],
-                category: 'Utility',
-                icon: './build/icons',
-            },
-            win: {
-                target: [
-                    {
-                        target: 'nsis',
-                        arch: ['x64'],
-                    },
-                    {
-                        target: 'portable',
-                        arch: ['x64'],
-                    },
-                    '7z',
-                ],
-                icon: './build/icons',
-            },
-            portable: {
-                artifactName: '${productName}-${version}-portable.exe',
-                splashImage: './public/splash.bmp',
-            },
-            nsis: {
-                oneClick: false,
-                perMachine: true,
-                allowElevation: true,
-                allowToChangeInstallationDirectory: true,
-                createDesktopShortcut: true,
-                createStartMenuShortcut: true,
-                deleteAppDataOnUninstall: true,
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const WorkerPlugin = require('worker-plugin')
+module.exports = {
+    configureWebpack: {
+        plugins: [new WorkerPlugin()],
+    },
+    pluginOptions: {
+        electronBuilder: {
+            nodeIntegration: true,
+            nodeGypRebuild: true,
+            externals: [
+                'mongoose',
+                'mongodb',
+                'vue3-qrcode-reader',
+                'vue-toastification',
+            ],
+            homepage: 'https://github.com/vedaprakashms',
+            builderOptions: {
+                // options placed here will be merged with default configuration and passed to electron-builder
+                appId: 'io.personalbiblothika.desktop',
+                productName: 'peronal Lib App',
+                linux: {
+                    target: ['deb', 'AppImage', '7z'],
+                    category: 'Utility',
+                    icon: './build/icons',
+                },
+                win: {
+                    target: [
+                        {
+                            target: 'nsis',
+                            arch: ['x64'],
+                        },
+                        {
+                            target: 'portable',
+                            arch: ['x64'],
+                        },
+                        '7z',
+                    ],
+                    icon: './build/icons',
+                },
+                portable: {
+                    artifactName: '${productName}-${version}-portable.exe',
+                    splashImage: './public/splash.bmp',
+                },
+                nsis: {
+                    oneClick: false,
+                    perMachine: true,
+                    allowElevation: true,
+                    allowToChangeInstallationDirectory: true,
+                    createDesktopShortcut: true,
+                    createStartMenuShortcut: true,
+                    deleteAppDataOnUninstall: true,
+                },
             },
         },
     },
