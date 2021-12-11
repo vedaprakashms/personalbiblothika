36bc5eb9394a560de07cfe8cb6cfb0135fb900ae
Vedaprakash <vedaprakashms@gmail.com>
2021-12-11 20:29:57 +0530

    update

---
+++ vue.config.js
@@ -0,0 +1,54 @@
+import WorkerPlugin from 'worker-plugin'
+export const configureWebpack = {
+    plugins: [new WorkerPlugin()],
+}
+export const pluginOptions = {
+    electronBuilder: {
+        nodeIntegration: true,
+        nodeGypRebuild: true,
+        externals: [
+            'mongoose',
+            'mongodb',
+            'vue3-qrcode-reader',
+            'vue-toastification',
+        ],
+        homepage: 'https://github.com/vedaprakashms',
+        builderOptions: {
+            // options placed here will be merged with default configuration and passed to electron-builder
+            appId: 'io.personalbiblothika.desktop',
+            productName: 'peronal Lib App',
+            linux: {
+                target: ['deb', 'AppImage', '7z'],
+                category: 'Utility',
+                icon: './build/icons',
+            },
+            win: {
+                target: [
+                    {
+                        target: 'nsis',
+                        arch: ['x64'],
+                    },
+                    {
+                        target: 'portable',
+                        arch: ['x64'],
+                    },
+                    '7z',
+                ],
+                icon: './build/icons',
+            },
+            portable: {
+                artifactName: '${productName}-${version}-portable.exe',
+                splashImage: './public/splash.bmp',
+            },
+            nsis: {
+                oneClick: false,
+                perMachine: true,
+                allowElevation: true,
+                allowToChangeInstallationDirectory: true,
+                createDesktopShortcut: true,
+                createStartMenuShortcut: true,
+                deleteAppDataOnUninstall: true,
+            },
+        },
+    },
+}
